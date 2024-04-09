cb1374c7812fba97b76a6a18887852d65bade326
Vipin <kumar.vipin1v00@gmail.com>
2024-04-09 10:04:13 +0530

    fixing gemini vision

--- app.py
+++ app.py
@@ -161,10 +161,12 @@ def NLP():
         st.header("Gemini Application")
         input=st.text_input("Input Prompt: ",key="input_prompt")
         uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-        image=""   
+        image=""  
+
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
-            st.image(image, caption="Uploaded Image.", use_column_width=True)
+            st.image(image, caption="Uploaded Image.", use_column_width=True) 
+       
         submit=st.button("Tell me about the image")
         ## If ask button is clicked
         if submit:
